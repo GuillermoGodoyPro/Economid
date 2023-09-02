423ce7a249ff3619946c76ae2648ad266ce787be
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-09-01 23:06:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+/* import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+ */
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1>Economid</h1>
+       
+      </div>
+      
+    </>
+  )
+}
+
+export default App
