631b9097e533cf566f87ae405d3ddde7d432faa9
guillermogodoypro <guillermo.rgo92@gmail.com>
2023-09-04 14:57:07 -0300

    update private routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import ForgotPassword from "./pages/ForgotPassword"
 import ConfirmAccount from "./pages/ConfirmAccount"
+import ProtectedPath from "./layouts/ProtectedPath"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
  
@@ -16,10 +18,15 @@ function App() {
               <Route path="signup" element={ <SignUp/>} />
               {/* Según doc: si agregamos forgotpassword/:token o confirm/:id podemos hacerlo dinámico para que el usuario recupere contraseña o confirme */}
               <Route path="forgotpassword" element={ <ForgotPassword/>} />
-              <Route path="confirm/:id" element={ <ConfirmAccount/>} />
+              <Route path="confirm/:id" element={ <ConfirmAccount/>} />        
          
-         
-         
+          </Route>
+
+          {/* Dentro de la ruta privada, cuando el usuario esta autenticado, hay llamar a otro grupo de rutas */}
+          {/* para invocar el dashboard, dentro de una ruta protegida (para proteger el resto de los componentes), todo lo que esté debajo de ruta protegida */}
+          
+          <Route path="/dashboard" element={ <ProtectedPath/> }>
+            <Route index element={<Dashboard />}/>
           </Route>
 
 
---
+++ src/layouts/ProtectedPath.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProtectedPath = () => {
+  return (
+    <div>ProtectedPath</div>
+  )
+}
+
+export default ProtectedPath
