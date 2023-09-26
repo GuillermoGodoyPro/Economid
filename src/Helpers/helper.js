e08985614153f2aecb18002a458bff5beb4a943c
ggodoy <guillermo.rgo92@gmail.com>
2023-09-25 21:51:01 -0300

    update singup para registrar usuarios y realizar conexiones

--- src/Helpers/helper.js
+++ src/Helpers/helper.js
@@ -1,3 +1,5 @@
-/* TODO: Realizar conexiones de css.modules de protected path, dashboard, forgotpassword, login, newpassword, perfil*/
+/*DONE -> TODO: Realizar conexiones de css.modules de protected path, dashboard, forgotpassword, login, newpassword, perfil*/
 /* Windows + shift + S para tomar captura en vsc */
 /* TODO: Hacer los USER STORY de layouts e IDEX.css*/
+/* TODO: Arreglar mediaquerys*/
+/* TODO: Arreglar los states de "contraseña, setPassword y cambiar a setContraseña*/
