2594c58db4efa9ef66d3f3394975e26d876acbca
ggodoy <guillermo.rgo92@gmail.com>
2023-10-11 17:41:53 -0300

    update Header and Sidebar base finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import burger from '../assets/cerrar.svg'
+
 
 const Header = () => {
   return (
-    <header className='px-4 py-5 border-b md:justify-between '>
-    
-      <div className='flex items-center gap-3 justify-between'>
+    <>
+      <header className='border-b md:justify-between '>        
         
-        <Link 
-          to="/perfil"
-          className='text-violet-600 font-bold uppercase'
-        >        
-          Perfil
-        </Link>
-        <button
-          type="button"
-          className='text-white text-sm bg-violet-400 p-3 rounded-md uppercase font-bold'
-        >
-          Cerrar Sesión
-        </button>
-      </div>
-    </header>
+          <div className='mt-1 px-4 py-5  flex items-center gap-3 justify-between'>
+           
+
+            <Link 
+              to="/perfil"
+              className='text-violet-600 font-bold uppercase'
+            >        
+              Perfil
+            </Link>
+            <button
+              type="button"
+              className='text-white text-sm bg-violet-400 p-3 rounded-md uppercase font-bold'
+            >
+              Cerrar Sesión
+            </button>
+          </div>
+        
+      </header>
+    </>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,47 +1,52 @@
 import React from 'react'
 import logoparaMF from '../assets/logoparaMF.png'
 import burger from '../assets/cerrar.svg'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-    <div className=" bg-violet-400 h-screen border-r py-4 px-2 flex">
-        <div className="p-2">
+    <div className=" bg-violet-400 h-screen border-r px-2 flex">
+        <div className="px-2">
             <div className="flex items-center mb-4">
                 <div>
-                    <div className='flex justify-end'>
+                    <div className='flex justify-end bg-violet-400 rounded '>
                         <img 
                             src={burger}
-                            className='flex w-4 h-4 rounded-3xl'
-                            />        
-                    </div>
+                            className='flex w-4 h-4 rounded-3xl mt-2'
+                        />        
+                    </div>  
 
-                    <div class="mt-1 overflow-hidden flex justify-between">
+                    <div class=" mr-2 mt-2 overflow-hidden flex justify-center">
                         <img className='rounded-2xl w-12' src="src/assets/logoparaMF.png" alt="Logo"/>
-                        <h2 className=" mt-1 flex ml-2 mr-10 text-white text-2xl font-semibold">MyFinance</h2>                        
                     </div>                   
+                    <h2 className=" mt-2 flex ml-2 mr-4 text-white text-2xl font-semibold">MyFinance</h2>                        
 
 
                 </div>
                 
             </div>
+                <div className='mt-6 block'>                    
+                    
+                    <Link className="mb-6 ml-10">
+                        <a href="#" className="text-xl text-white hover:text-gray-200 font-bold">Item</a>
+                    </Link>
+                    <Link className="mb-6 ml-10">
+                        <a href="#" className="text-xl text-white hover:text-gray-200 font-bold">Item</a>
+                    </Link>
+                    <Link className="mb-6 ml-10">
+                        <a href="#" className="text-xl text-white hover:text-gray-200 font-bold">Item</a>
+                    </Link>
+                    <Link className="mb-6 ml-10">
+                        <a href="#" className="text-xl text-white hover:text-gray-200 font-bold">Item</a>
+                    </Link>
+                    <Link className="mb-6 ml-10">
+                        <a href="#" className="text-xl text-white hover:text-gray-200 font-bold">Item</a>
+                    </Link>           
 
+                </div>
                 
 
                 
-                <ul className="mt-4">
-                    <li className="mb-2">
-                        <a href="#" className="text-xl text-white hover:text-gray-200 ">Item</a>
-                    </li>
-                    <li className="mb-2">
-                        <a href="#" className="text-xl text-white hover:text-gray-200">Item</a>
-                    </li>
-                    <li className="mb-2">
-                        <a href="#" className="text-xl text-white hover:text-gray-200">Item</a>
-                    </li>
-                    <li className="mb-2">
-                        <a href="#" className="text-xl text-white hover:text-gray-200">Item</a>
-                    </li>
-                </ul>
         </div>
     </div>
     
