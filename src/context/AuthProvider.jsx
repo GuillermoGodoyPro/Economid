818404b68b261ad9721d5ab1201a5c11ffb89e3e
samuelerm08 <samuelerm08@gmail.com>
2023-10-28 02:01:11 -0300

    cambios

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import clienteAxios from "../config/clienteAxios";
-import { useNavigate } from "react-router-dom";
 
-
-//creo el AuthContext y lo utilizo en otros lugares
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
@@ -11,34 +7,16 @@ const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState("")
     const [cargando, setCargando] = useState(true)
 
-    const navigate = useNavigate()
-
-    /* este useEffect se ejecuta una sola vez para comprobar si hay token*/
     useEffect(() => {
-        const autenticarUsuario = async() => {
-        //TODO: Preguntar a Emilio si aca tengo que invocar el useFetch para no hacer todo esto
-
+        const autenticarUsuario = async () => {
             const token = localStorage.getItem('token')
-            
-            if(!token){
+            if (!token) {
                 setCargando(false)
-                /* setAuth(false) */
                 return
             }
 
-            // Para pasar el bearer token
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                }
-            }
-
             try {
-                await setAuth(token)
-                //TODO: Ruta a perfil económico           
-                /* navigate('/dashboard') */
-
+                setAuth(token)
             } catch (error) {
                 console.log(error)
                 setAuth({})
@@ -50,7 +28,7 @@ const AuthProvider = ({ children }) => {
 
     }, [])
 
-    return(
+    return (
         <AuthContext.Provider
             value={{
                 auth,
