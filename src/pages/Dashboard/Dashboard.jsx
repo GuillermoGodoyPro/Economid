3fd042fa351b9ddd9f710265184a478757c0f53f
samuelerm08 <samuelerm08@gmail.com>
2023-11-09 21:25:22 -0300

    agregar saldos

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -13,24 +13,24 @@ import { generarId } from '../../Helpers/helper';
 const Dashboard = () => {
 
 
-  const { auth } = useAuth()
+  const { auth } = useAuth();
   const [balance, setBalance] = useState(null);
   const [cargando, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const [modal, setModal] = useState(false)
-  const [animarModal, setAnimarModal] = useState(false)
-  const [transaction, saveTransaction] = useState([])
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [transaction, saveTransaction] = useState([]);
 
   // Inicio de Transacciones y PE -- conexión con Modal
 
   const handlePerfilEcon = () => {
-    setModal(true)
+    setModal(true);
 
     setTimeout(() => {
 
       setAnimarModal(true)
-    }, 400)
+    }, 400);
   }
   const guardarTransaccion = t => {
 
@@ -40,7 +40,7 @@ const Dashboard = () => {
     saveTransaction([...transaction, t])
   }
 
-  console.log(transaction)
+  console.log(transaction);
 
 
   // Fin de Transacciones y PE -- conexión con Modal
@@ -109,17 +109,19 @@ const Dashboard = () => {
       {/* Cabecera */}
       <div className=" bg-inherit rounded p-2 m-4 mb-0 flex justify-between">
         {/* TODO: Cambiar por ternario, copiar y pegar todo pero solo modificar el boton perfil económico por nueva transacción */}
-        <div className="bg-gray-200 p-4 rounded-lg shadow-sm w-full mr-1 ">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-sm w-full mr-1">
           {balance ?
-            <div>
-              <h2 className='p-1 justify-around mb-4 text-violet-600'>
+            <div className='flex justify-around'>
+              <h2 className='p-1 mb-4 text-violet-600'>
                 <span className='font-bold'>
-                  Saldo Inicial: {balance.data.saldo_Inicial}
+                  Saldo Inicial: <br />
+                  ${parseFloat(balance.data.saldo_Inicial).toFixed(2)}
                 </span>
               </h2>
-              <h2 className='p-1 justify-around mb-6 text-violet-600'>
+              <h2 className='p-1 mb-4 text-violet-600'>
                 <span className='font-bold'>
-                  Saldo Total: {balance.data.saldo_Total}
+                  Saldo Total: <br />
+                  ${parseFloat(balance.data.saldo_Total).toFixed(2)}
                 </span>
               </h2>
             </div> :
@@ -187,7 +189,30 @@ const Dashboard = () => {
       {/* Lista de gastos */}
 
       <div className="bg-inherit p-10">
-        <div className="">
+        <div className="bg-inherit p-4 rounded-lg shadow-md border">
+          <table className="w-full border-collapse">
+            <thead>
+              <tr>
+                <th className="text-left py-2 px-4 font-semibold text-violet-600">Transacción</th>
+                <th className="text-left py-2 px-4 font-semibold text-violet-600">Monto</th>
+                <th className="text-left py-2 px-4 font-semibold text-violet-600">Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b border-gray-200">
+                <td className="py-2 px-4">Panaderia</td>
+                <td className="py-2 px-4">$ 700</td>
+                <td className="py-2 px-4">05/06/2023</td>
+              </tr>
+              <tr className="border-b border-gray-200">
+                <td className="py-2 px-4">Alquiler</td>
+                <td className="py-2 px-4">$ 75000</td>
+                <td className="py-2 px-4">10/06/2023</td>
+              </tr>
+
+
+            </tbody>
+          </table>
         </div>
       </div>
 
@@ -273,13 +298,9 @@ const Dashboard = () => {
           </h2>
 
           <div className='flex justify-center'>
-            <h2 className='p-1 justify-around text-violet-600'>
-              Cálculo Patrimonio Neto:
-
-            </h2>
-            <h2 className='p-1 justify-around text-violet-800 font-bold uppercase'>
-              Pasivos - Activos
-            </h2>
+            <h1 className='p-1 justify-around text-violet-800 font-bold uppercase'>
+              ${parseFloat(balance.data.saldo_Total).toFixed(2)}
+            </h1>
 
           </div>
 
